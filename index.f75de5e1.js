a50c141fb410c85a5a524926f356cc482d2c9432
SrTrace <levashov04sergey@gmail.com>
2024-06-06 21:05:13 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const populationElements = [
+    ...document.querySelectorAll(".population")
+];
+const totalElement = document.querySelector(".total-population");
+const averageElement = document.querySelector(".average-population");
+function parseNumber(str) {
+    const clearComaStr = str.replace(/,/g, "");
+    const number = Number(clearComaStr);
+    return isNaN(number) ? 0 : number;
+}
+const totalPopulation = populationElements.reduce((sum, el)=>{
+    const numberOfPeople = parseNumber(el.innerText);
+    return sum + numberOfPeople;
+}, 0);
+const numberFormatter = Intl.NumberFormat("en-US");
+const formattedTotal = numberFormatter.format(totalPopulation);
+const averagePopulation = totalPopulation / populationElements.length;
+const formattedAverage = numberFormatter.format(averagePopulation);
+totalElement.innerText = formattedTotal;
+averageElement.innerText = formattedAverage;
+
+//# sourceMappingURL=index.f75de5e1.js.map
